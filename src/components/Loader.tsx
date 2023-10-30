1129d936adf85249a7694560e463716143ac5fd2
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-10-30 11:06:11 +0330

    updated loader text

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,18 +1,18 @@
-import { LoaderIcon } from "react-hot-toast";
+import { LoaderIcon } from 'react-hot-toast';
 
 function Loader() {
   return (
     <div
       style={{
-        color: "var(--primary-600)",
-        display: "flex",
-        alignItems: "center",
-        gap: "1rem",
-        margin: "1rem auto",
+        color: 'var(--primary-600)',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '1rem',
+        margin: '1rem auto',
       }}
     >
-      <p> Loading Data...</p>
-      <LoaderIcon style={{ width: "1.3rem", height: "1.3rem" }} />
+      <p>در حال بارگذاری ...</p>
+      <LoaderIcon style={{ width: '1.3rem', height: '1.3rem' }} />
     </div>
   );
 }
